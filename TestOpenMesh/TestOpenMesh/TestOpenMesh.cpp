e2ddde676bdef6b51c8e12854e483634ec49c90c
touwoyimuli@gmail.com <touwoyimuli@gmail.com>
2019-06-21 15:23:19 +0800

    update

---
+++ TestOpenMesh/TestOpenMesh/TestOpenMesh.cpp
@@ -0,0 +1,84 @@
+// TestOpenMesh.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+// -------------------- OpenMesh
+#include <OpenMesh\/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh>
+using namespace std;
+
+typedef OpenMesh::PolyMesh_ArrayKernelT<>  MyMesh;
+int main()
+{
+	MyMesh mesh;
+	MyMesh::VertexHandle vhandle[8];
+	vhandle[0] = mesh.add_vertex(MyMesh::Point(-1, -1, 1));
+	vhandle[1] = mesh.add_vertex(MyMesh::Point(1, -1, 1));
+	vhandle[2] = mesh.add_vertex(MyMesh::Point(1, 1, 1));
+	vhandle[3] = mesh.add_vertex(MyMesh::Point(-1, 1, 1));
+	vhandle[4] = mesh.add_vertex(MyMesh::Point(-1, -1, -1));
+	vhandle[5] = mesh.add_vertex(MyMesh::Point(1, -1, -1));
+	vhandle[6] = mesh.add_vertex(MyMesh::Point(1, 1, -1));
+	vhandle[7] = mesh.add_vertex(MyMesh::Point(-1, 1, -1));
+	// generate (quadrilateral) faces
+	std::vector<MyMesh::VertexHandle>  face_vhandles;
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[0]);
+	face_vhandles.push_back(vhandle[1]);
+	face_vhandles.push_back(vhandle[2]);
+	face_vhandles.push_back(vhandle[3]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[7]);
+	face_vhandles.push_back(vhandle[6]);
+	face_vhandles.push_back(vhandle[5]);
+	face_vhandles.push_back(vhandle[4]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[1]);
+	face_vhandles.push_back(vhandle[0]);
+	face_vhandles.push_back(vhandle[4]);
+	face_vhandles.push_back(vhandle[5]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[2]);
+	face_vhandles.push_back(vhandle[1]);
+	face_vhandles.push_back(vhandle[5]);
+	face_vhandles.push_back(vhandle[6]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[3]);
+	face_vhandles.push_back(vhandle[2]);
+	face_vhandles.push_back(vhandle[6]);
+	face_vhandles.push_back(vhandle[7]);
+	mesh.add_face(face_vhandles);
+
+	face_vhandles.clear();
+	face_vhandles.push_back(vhandle[0]);
+	face_vhandles.push_back(vhandle[3]);
+	face_vhandles.push_back(vhandle[7]);
+	face_vhandles.push_back(vhandle[4]);
+	mesh.add_face(face_vhandles);
+
+	// write mesh to output.obj
+	try
+	{
+		if (!OpenMesh::IO::write_mesh(mesh, "output.off"))
+		{
+			std::cerr << "Cannot write mesh to file 'output.off'" << std::endl;
+			return 1;
+		}
+	}
+	catch (std::exception& x)
+	{
+		std::cerr << x.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
+}
